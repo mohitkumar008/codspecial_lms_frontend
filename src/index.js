0f070f1d9a3ca463b1fd2cff90ab7471b03de054
Mohit Kumar <77313561+mohitkumar008@users.noreply.github.com>
2023-08-18 05:56:27 +0000

    some bug fixes

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import { store } from 'store';
 
 // style + assets
 import 'assets/scss/style.scss';
-import config from './config';
+// import config from './config';
 
 // ==============================|| REACT DOM RENDER  ||============================== //
 
@@ -19,7 +19,7 @@ const container = document.getElementById('root');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
 root.render(
   <Provider store={store}>
-    <BrowserRouter basename={config.basename}>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </Provider>
